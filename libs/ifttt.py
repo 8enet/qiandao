03cabc6efc54c679a04eb1895664ccf56e9fd97b
8enet <zlcn2200@yeah.net>
2017-09-19 12:19:46 +0800

    async request

--- libs/ifttt.py
+++ libs/ifttt.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 
-import requests
 import time
 import logging
 import config
 import sys
+import json
 
+from tornado import httpclient
+from tornado.httpclient import AsyncHTTPClient, HTTPClient
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -19,7 +21,16 @@ def notify_web(task, tpl, err = None):
     msg = "%s%s %s" % (tpl['sitename'], task['note'], "签到成功" if err is None else ("签到失败:%s" % unicode(err)) )
     data = {"value1": msg, "value2": localtime}
     logger.info("notify itfff msg %s " % msg)
-    r = requests.post('https://maker.ifttt.com/trigger/%s/with/key/%s' % (config.ifttt_event, config.ifttt_key), json=data,
-                      headers={'Connection': 'close'}, timeout=180)
-    logger.info(r.headers)
-    logger.info(r.content)
+    # r = requests.post('https://maker.ifttt.com/trigger/%s/with/key/%s' % (config.ifttt_event, config.ifttt_key), json=data,
+    #                   headers={'Connection': 'close'}, timeout=180)
+    # logger.info(r.headers)
+    # logger.info(r.content)
+    AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
+    http_client = AsyncHTTPClient()
+    req = httpclient.HTTPRequest(
+        method='POST',
+        url='https://maker.ifttt.com/trigger/%s/with/key/%s' % (config.ifttt_event, config.ifttt_key),
+        headers={'Content-Type': 'application/json','Connection': 'close'},
+        body=json.dumps(data)
+    )
+    http_client.fetch(req)
